d2fb1801ee801797a93057bbc25869a078c880b1
mateolopezlorenz <mlopezlorenz@cifpfbmoll.eu>
2026-02-27 13:07:21 +0100

    Se ha agregado la parte inicial del backend del proyecto.

---
+++ metricasocial/src/main/java/com/metricasocial/MetricaSocialApplication.java
@@ -0,0 +1,13 @@
+package com.metricasocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricaSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricaSocialApplication.class, args);
+	}
+
+}
